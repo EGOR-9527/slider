658fd7f6e5c09988808f8a7f2f7a521988886018
Egor Docenko <egordocenko359@gmail.com>
2024-12-13 23:36:49 +0300

    Измененный дизайн

--- src/shared/Loader.tsx
+++ src/shared/Loader.tsx
@@ -2,15 +2,23 @@
 import React from 'react';
 import { Player } from '@lottiefiles/react-lottie-player';
 import loadingAnimation from '../animation/Loader.json'; // Укажите путь к вашему файлу Lottie
+import Welcometo from '../animation/Welcometo.json'; // Укажите путь к вашему файлу Lottie
 
 const Loader: React.FC = () => {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', position: "absolute" ,top: "-20%",}}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', position: "absolute", top: "-15vw", overflow: 'hidden'}}>
             <Player
                 autoplay
                 loop
                 src={loadingAnimation}
-                style={{ height:"100vw", width: "100vw"}} // Укажите размеры лоадера
+                style={{ height: "100vw", width: "100vw"}} // Укажите размеры лоадера
+            />
+
+            <Player
+                autoplay
+                loop
+                src={Welcometo}
+                style={{ height: "100vw", width: "100vw", position: "absolute", marginTop: "0px", left: "0"}} // Укажите размеры лоадера
             />
         </div>
     )
